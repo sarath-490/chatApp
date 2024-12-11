1edd0141bc02da63b44181c3ea070481961107c0
Redrouthu sarath sai <sarath2005sai0505@gmail.com>
2024-12-11 18:02:43 +0530

    final commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ main().then(()=>{console.log("Mongodb connected succesfully.")}).
 catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/online_backend_chat');
+  await mongoose.connect('mongodb+srv://sarath2005sai0505:VLuMFpz5vqm5aFbi@cluster0.tfgtp.mongodb.net/online_backend_chat?retryWrites=true&w=majority');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
